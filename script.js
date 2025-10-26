65488afd71c988422614ebff555825589cfd352c
Abhijeet Anand <abhijeetanand628@gmail.com>
2025-10-26 17:23:51 +0530

    Declared all the DOMs

---
+++ script.js
@@ -0,0 +1,5 @@
+const lvl = document.querySelector('#level-display');
+const startBtn = document.querySelector('#start-btn');
+const buttons = document.querySelectorAll('.simon-btn');
+
+lvl.innerHTML = '0';
